b5bf27ad92aa287014b16886f7476f89fcc3a598
sumbad <sumbad@ya.ru>
2017-08-31 20:33:05 +0800

    Updates

--- js/app.js
+++ js/app.js
@@ -4903,10 +4903,7 @@ var ArticlesListComponent = (function (_super) {
     ArticlesListComponent.prototype._loadPreviews = function (names) {
         return __awaiter(this, void 0, void 0, function () {
             return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0: return [4, Promise.all([new Promise(function (resolve) { return resolve('<div>111111111</div>'); }), new Promise(function (resolve) { return resolve('<div>111111111</div>'); })])];
-                    case 1: return [2, _a.sent()];
-                }
+                return [2, Promise.all([new Promise(function (resolve) { return resolve('<div>111111111</div>'); }), new Promise(function (resolve) { return resolve('<div>111111111</div>'); })])];
             });
         });
     };
