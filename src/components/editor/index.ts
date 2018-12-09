da200d6089a644fee9479d360c43ceb77d2845ec
sumbad <sumbad@ya.ru>
2018-12-09 18:10:43 +0800

    create editor on quilljs lib

--- src/components/editor/index.ts
+++ src/components/editor/index.ts
@@ -1,9 +1,38 @@
 import { TemplateResult } from 'lit-html';
-import { Define, AbstractElement } from 'abstract-element';
+import { Define, AbstractElement, state } from 'abstract-element';
 import * as litHtml from 'lit-html';
 import litRender from 'abstract-element/render/lit';
 
 import Quill from './quill-register';
+import Block from 'quill/blots/block';
+
+import fontawesome from '@fortawesome/fontawesome';
+import faBold from '@fortawesome/fontawesome-free-solid/faBold';
+import faItalic from '@fortawesome/fontawesome-free-solid/faItalic';
+import faLink from '@fortawesome/fontawesome-free-solid/faLink';
+import faQuoteRight from '@fortawesome/fontawesome-free-solid/faQuoteRight';
+import faHeading from '@fortawesome/fontawesome-free-solid/faHeading';
+import faCamera from '@fortawesome/fontawesome-free-solid/faCamera';
+import faPlay from '@fortawesome/fontawesome-free-solid/faPlay';
+import faMinus from '@fortawesome/fontawesome-free-solid/faMinus';
+import faPlus from '@fortawesome/fontawesome-free-solid/faPlus';
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
+import BlockBlot from 'parchment/dist/src/blot/block';
+
+
+
+fontawesome.library.add(
+  faBold,
+  faItalic,
+  faLink,
+  faQuoteRight,
+  faHeading,
+  faCamera,
+  faPlay,
+  faTwitter,
+  faMinus,
+  faPlus
+);
 
 
 /**
@@ -18,6 +47,9 @@ export class DemoLitComponent extends AbstractElement {
     time: new Date().toLocaleTimeString()
   };
 
+  @state()
+  active: boolean = false;
+
 
   constructor() {
     super(litRender, false);
@@ -26,29 +58,190 @@ export class DemoLitComponent extends AbstractElement {
 
   connectedCallback() {
     super.connectedCallback();
-    console.log(this.querySelector('#editor'), 'connectedCallback');
 
-    var quill = new Quill(this.querySelector('#editor'), {
-      theme: 'snow'
+    const editorContainerEl = this.querySelector('#editor-container') as HTMLDivElement;
+    const tooltipControlsEl = this.querySelector('#tooltip-controls') as HTMLDivElement;
+    const sidebarControlsEl = this.querySelector('#sidebar-controls') as HTMLDivElement;
+    const showControlsEl = this.querySelector('#show-controls') as HTMLDivElement;
+    let quill = new Quill(editorContainerEl);
+    quill.addContainer(tooltipControlsEl);
+    quill.addContainer(sidebarControlsEl);
+
+
+    quill.on('editor-change', (eventType, range) => {
+      if (eventType !== 'selection-change') { return; }
+      if (range == null) { return; }
+      if (range.length === 0) {
+        // $('#tooltip-controls').hide();
+        tooltipControlsEl.style.display = 'none';
+        let [block, offset] = quill.scroll.scroll.descendant<BlockBlot>(Block as any, range.index);
+        if (block != null && block.domNode.firstChild instanceof HTMLBRElement) {
+          let lineBounds = quill.getBounds(range);
+          // $('#sidebar-controls').removeClass('active').show().css({
+          //   left: lineBounds.left - 50,
+          //   top: lineBounds.top - 2
+          // });
+          sidebarControlsEl.classList.remove('active');
+          this.active = false;
+          sidebarControlsEl.style.display = 'initial';
+          sidebarControlsEl.style.left = `${lineBounds.left - 80}px`;
+          sidebarControlsEl.style.top = `${lineBounds.top - 5}px`;
+        } else {
+          // $('#tooltip-controls, #sidebar-controls').hide();
+          tooltipControlsEl.style.display = 'none';
+          // $('#sidebar-controls').removeClass('active');
+          // sidebarControlsEl.classList.remove('active');
+          sidebarControlsEl.style.display = 'none';
+        }
+      } else {
+        // $('#sidebar-controls, #sidebar-controls').hide();
+        // $('#sidebar-controls').removeClass('active');
+        sidebarControlsEl.style.display = 'none';
+        sidebarControlsEl.classList.remove('active');
+        this.active = false;
+
+        let rangeBounds = quill.getBounds(range);
+        // $('#tooltip-controls').show().css({
+        //   left: rangeBounds.left + rangeBounds.width / 2 - $('#tooltip-controls').outerWidth() / 2,
+        //   top: rangeBounds.bottom + 10
+        // });
+        tooltipControlsEl.style.display = 'initial';
+        tooltipControlsEl.style.left = `${rangeBounds.left + rangeBounds.width / 2 - tooltipControlsEl.offsetWidth / 2}px`;
+        tooltipControlsEl.style.top = `${rangeBounds.bottom + 10}px`;
+      }
     });
 
-    console.log(quill.getContents());
-    console.log(quill.getText(0, quill.getLength()));
+
+
+    const boldButtonEl = this.querySelector('#bold-button') as HTMLButtonElement;
+    const italicButtonEl = this.querySelector('#italic-button') as HTMLButtonElement;
+    const linkButtonEl = this.querySelector('#link-button') as HTMLButtonElement;
+    const blockquoteButtonEl = this.querySelector('#blockquote-button') as HTMLButtonElement;
+    const header1ButtonEl = this.querySelector('#header-1-button') as HTMLButtonElement;
+    const header2ButtonEl = this.querySelector('#header-2-button') as HTMLButtonElement;
+    const dividerButtonEl = this.querySelector('#divider-button') as HTMLButtonElement;
+    const imageButtonEl = this.querySelector('#image-button') as HTMLButtonElement;
+    const videoButtonEl = this.querySelector('#video-button') as HTMLButtonElement;
+    const tweetButtonEl = this.querySelector('#tweet-button') as HTMLButtonElement;
+
+
+    boldButtonEl.addEventListener('click', () => {
+      quill.format('bold', true);
+    });
+
+    italicButtonEl.addEventListener('click', () => {
+      quill.format('italic', true);
+    });
+
+    linkButtonEl.addEventListener('click', () => {
+      let value = prompt('Enter link URL');
+      quill.format('link', value);
+    });
+
+    blockquoteButtonEl.addEventListener('click', () => {
+      quill.format('blockquote', true);
+    });
+
+    header1ButtonEl.addEventListener('click', () => {
+      quill.format('header', 1);
+    });
+
+    header2ButtonEl.addEventListener('click', () => {
+      quill.format('header', 2);
+    });
+
+    dividerButtonEl.addEventListener('click', () => {
+      let range = quill.getSelection(true);
+      quill.insertEmbed(range.index, 'divider', true, Quill.sources.USER);
+      quill.setSelection(range.index + 1, 0, Quill.sources.SILENT);
+      sidebarControlsEl.style.display = 'none';
+    });
+
+    imageButtonEl.addEventListener('click', () => {
+      let range = quill.getSelection(true);
+      quill.insertEmbed(range.index, 'image', {
+        alt: 'Quill Cloud',
+        url: 'https://quilljs.com/0.20/assets/images/cloud.png'
+      }, Quill.sources.USER);
+      quill.setSelection(range.index + 1, 0, Quill.sources.SILENT);
+      sidebarControlsEl.style.display = 'none';
+    });
+
+    videoButtonEl.addEventListener('click', () => {
+      let range = quill.getSelection(true);
+      let url = 'https://www.youtube.com/embed/QHH3iSeDBLo?showinfo=0';
+      quill.insertEmbed(range.index, 'video', url, Quill.sources.USER);
+      quill.formatText(range.index + 1, 1, { height: '170', width: '400' });
+      quill.setSelection(range.index + 1, 0, Quill.sources.SILENT);
+      sidebarControlsEl.style.display = 'none';
+    });
+
+    tweetButtonEl.addEventListener('click', () => {
+      let range = quill.getSelection(true);
+      let id = '464454167226904576';
+      quill.insertEmbed(range.index, 'tweet', id, Quill.sources.USER);
+      quill.setSelection(range.index + 1, 0, Quill.sources.SILENT);
+      sidebarControlsEl.style.display = 'none';
+    });
+
+
+    showControlsEl.addEventListener('click', () => {
+      this.active = !this.active;
+      sidebarControlsEl.classList.toggle('active');
+      // quill.focus();
+      console.log(quill.getContents());
+      console.log(quill.root.innerHTML);
+    });
+
+
+    window['quill'] = quill;
   }
 
 
   render(): TemplateResult {
-    console.log(111, this.css);
-    
+    const style = this.html`<style>${this.css}</style>`;
+    const showControlsIcon = this.active
+      ? this.html`<i class="fas fa-plus" data-fa-transform="rotate-45"></i>`
+      : this.html`<i class="fas fa-plus"></i>`;
+
     return this.html`
-      <style>
-        ${this.css}
-      </style>
+      ${style}
       
-      <div id="editor">
-        <p>Hello World!</p>
-        <p>Some initial <strong>bold</strong> text</p>
-        <p><br></p>
+      <div id="tooltip-controls">
+        <button id="bold-button"><i class="fas fa-bold"></i></button>
+        <button id="italic-button"><i class="fas fa-italic"></i></button>
+        <button id="link-button"><i class="fas fa-link"></i></button>
+        <button id="blockquote-button"><i class="fas fa-quote-right"></i></button>
+        <button id="header-1-button">
+          <span class="fa-layers fa-fw">
+            <i class="fas fa-heading"></i>
+            <span class="fa-layers-text" data-fa-transform="shrink-6 down-4 right-11">
+              1
+            </span>
+          </span>
+        </button>
+        <button id="header-2-button">
+          <span class="fa-layers fa-fw">
+            <i class="fas fa-heading"></i>
+            <span class="fa-layers-text" data-fa-transform="shrink-6 down-4 right-11">
+              2
+            </span>
+          </span>
+        </button>
+      </div>
+      <div id="sidebar-controls">
+        <button id="show-controls">
+          ${showControlsIcon}
+        </button>
+        <span class="controls">
+          <button id="image-button"><i class="fas fa-camera"></i></button>
+          <button id="video-button"><i class="fas fa-play"></i></button>
+          <button id="tweet-button"><i class="fab fa-twitter"></i></button>
+          <button id="divider-button"><i class="fas fa-minus"></i></button>
+        </span>
+      </div>
+      <div id="editor-container">
+        <p>Tell your story...<p>
       </div>
     `;
   }
--- src/components/editor/quill-register.ts
+++ src/components/editor/quill-register.ts
@@ -1,21 +1,184 @@
 import Quill from 'quill/core';
-
-import Toolbar from 'quill/modules/toolbar';
-import Snow from 'quill/themes/snow';
-
-import Bold from 'quill/formats/bold';
-import Italic from 'quill/formats/italic';
-import Header from 'quill/formats/header';
-import Underline from 'quill/formats/underline';
+import Inline from 'quill/blots/inline';
+import Block from 'quill/blots/block';
+import { BlockEmbed } from 'quill/blots/block';
 
 
-Quill.register({
-  'modules/toolbar': Toolbar,
-  'themes/snow': Snow,
-  'formats/bold': Bold,
-  'formats/italic': Italic,
-  'formats/header': Header,
-  'formats/underline': Underline
+
+class BoldBlot extends Inline { };
+
+BoldBlot.blotName = 'bold';
+BoldBlot.tagName = 'strong';
+
+class ItalicBlot extends Inline { }
+ItalicBlot.blotName = 'italic';
+ItalicBlot.tagName = 'em';
+
+Quill.register(BoldBlot);
+Quill.register(ItalicBlot);
+
+
+
+class LinkBlot extends Inline {
+  static create(value) {
+    let node = super.create(value) as Element;
+    // Sanitize url value if desired
+    node.setAttribute('href', value);
+    // Okay to set other non-format related attributes
+    // These are invisible to Parchment so must be static
+    node.setAttribute('target', '_blank');
+    return node;
+  }
+
+  static formats(node) {
+    // We will only be called with a node already
+    // determined to be a Link blot, so we do
+    // not need to check ourselves
+    return node.getAttribute('href');
+  }
+}
+LinkBlot.blotName = 'link';
+LinkBlot.tagName = 'a';
+
+Quill.register(LinkBlot);
+
+
+class BlockquoteBlot extends Block { }
+BlockquoteBlot.blotName = 'blockquote';
+BlockquoteBlot.tagName = 'blockquote';
+
+Quill.register(BlockquoteBlot);
+
+
+class HeaderBlot extends Block {
+  // static formats(node) {
+  //   return HeaderBlot.tagName.indexOf(node.tagName) + 1;
+  // }
+}
+HeaderBlot.blotName = 'header';
+HeaderBlot.tagName = ['h1', 'h2'] as any;
+
+Quill.register(HeaderBlot);
+
+
+class DividerBlot extends BlockEmbed { }
+DividerBlot.blotName = 'divider';
+DividerBlot.tagName = 'hr';
+
+Quill.register(DividerBlot);
+
+
+class ImageBlot extends BlockEmbed {
+  static create(value) {
+    let node = super.create(null) as Element;
+    node.setAttribute('alt', value.alt);
+    node.setAttribute('src', value.url);
+    return node;
+  }
+
+  static value(node) {
+    return {
+      alt: node.getAttribute('alt'),
+      url: node.getAttribute('src')
+    };
+  }
+}
+ImageBlot.blotName = 'image';
+ImageBlot.tagName = 'img';
+
+Quill.register(ImageBlot);
+
+
+class VideoBlot extends BlockEmbed {
+  domNode: any;
+  static create(url) {
+    let node = super.create(this.value) as Element;
+    node.setAttribute('src', url);
+    node.setAttribute('frameborder', '0');
+    node.setAttribute('allowfullscreen', 'true');
+    return node;
+  }
+
+  static formats(node) {
+    let format: { height?: string, width?: string } = {};
+    if (node.hasAttribute('height')) {
+      format.height = node.getAttribute('height');
+    }
+    if (node.hasAttribute('width')) {
+      format.width = node.getAttribute('width');
+    }
+    return format;
+  }
+
+  static value(node) {
+    return node.getAttribute('src');
+  }
+
+  format(name, value) {
+    if (name === 'height' || name === 'width') {
+      if (value) {
+        this.domNode.setAttribute(name, value);
+      } else {
+        this.domNode.removeAttribute(name, value);
+      }
+    } else {
+      super.format(name, value);
+    }
+  }
+}
+VideoBlot.blotName = 'video';
+VideoBlot.tagName = 'iframe';
+
+Quill.register(VideoBlot);
+
+
+
+
+function getTwttr(d, s, id) {
+  let js, fjs = d.getElementsByTagName(s)[0];
+  let t = window['twttr'] || {};
+
+  if (d.getElementById(id)) return t;
+
+  js = d.createElement(s);
+  js.id = id;
+  js.src = 'https://platform.twitter.com/widgets.js';
+  (fjs.parentNode as Node).insertBefore(js, fjs);
+
+  return new Promise((resolve, reject) => {
+    let timerId = setTimeout(function tick() {
+      t = window['twttr'];
+      if (t === undefined) {
+        timerId = setTimeout(tick, 500);
+      } else {
+        resolve(t);
+      }
+    }, 500);
+  });
+}
+
+
+class TweetBlot extends BlockEmbed {
+  static twttr;
+  static create(id) {
+    let node = super.create(id) as any;
+    node.dataset.id = id;
+    TweetBlot.twttr.widgets.createTweet(id, node);
+    return node;
+  }
+
+  static value(domNode) {
+    return domNode.dataset.id;
+  }
+}
+
+getTwttr(document, 'script', 'twitter-wjs').then(t => {
+  TweetBlot.blotName = 'tweet';
+  TweetBlot.tagName = 'div';
+  TweetBlot.className = 'tweet';  
+  TweetBlot.twttr = t;
+
+  Quill.register(TweetBlot);
 });
 
 
