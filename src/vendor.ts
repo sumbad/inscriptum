e58099a03d1464df872411d9455b20f2ad2e469a
sumbad <sumbad@ya.ru>
2019-04-20 22:45:35 +0800

    change vendor chunk

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,16 +1,18 @@
 // RxJS
-import 'rxjs';
+// import 'rxjs';
 import 'rxjs/Observable'
 import 'rxjs/Observer'
 import 'rxjs/Subject'
-import 'rxjs/add/operator/share'
-import 'rxjs/add/operator/map'
-import 'rxjs/add/operator/catch'
-import 'rxjs/add/observable/throw'
-import 'rxjs/add/operator/debounceTime'
-import 'rxjs/add/operator/distinctUntilChanged'
-import 'rxjs/add/operator/switchMap'
-import 'rxjs/add/operator/delay'
+// import 'rxjs/add/operator/share'
+// import 'rxjs/add/operator/map'
+// import 'rxjs/add/operator/catch'
+// import 'rxjs/add/observable/throw'
+// import 'rxjs/add/operator/debounceTime'
+// import 'rxjs/add/operator/distinctUntilChanged'
+// import 'rxjs/add/operator/switchMap'
+// import 'rxjs/add/operator/delay'
 
 // https://github.com/visionmedia/page.js
 import 'page';
+
+import 'auth0-js';
