dcb259a87227a02d430cf127befbb28d21f555c5
sumbad <sumbad@ya.ru>
2019-09-04 09:55:44 +0800

    chore(notepad): add xml and sql to highlight support

--- src/components/editor/editor/highlightjs.ts
+++ src/components/editor/editor/highlightjs.ts
@@ -5,18 +5,22 @@ import highlightjs from 'highlight.js/lib/highlight'
 import javascript from 'highlight.js/lib/languages/javascript';
 import typescript from 'highlight.js/lib/languages/typescript';
 import html from 'highlight.js/lib/languages/htmlbars';
+import sql from 'highlight.js/lib/languages/sql';
+import xml from 'highlight.js/lib/languages/xml';
 import css from 'highlight.js/lib/languages/css';
 import bash from 'highlight.js/lib/languages/bash';
 import yaml from 'highlight.js/lib/languages/yaml';
 
 
 // Register languages
-highlightjs.registerLanguage('javascript', javascript)
-highlightjs.registerLanguage('typescript', typescript)
-highlightjs.registerLanguage('html', html)
-highlightjs.registerLanguage('css', css)
-highlightjs.registerLanguage('bash', bash)
-highlightjs.registerLanguage('yaml', yaml)
+highlightjs.registerLanguage('javascript', javascript);
+highlightjs.registerLanguage('typescript', typescript);
+highlightjs.registerLanguage('sql', sql);
+highlightjs.registerLanguage('html', html);
+highlightjs.registerLanguage('xml', xml);
+highlightjs.registerLanguage('css', css);
+highlightjs.registerLanguage('bash', bash);
+highlightjs.registerLanguage('yaml', yaml);
 
 
 export default highlightjs;
