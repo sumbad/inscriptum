6202c5f7880a26da377419826b6e12d2fe091747
sumbad <sumbad@ya.ru>
2020-01-20 19:37:03 +0800

    refactor(notepad): remove date of code

--- src/components/_editor/template.ts
+++
@@ -1,9 +0,0 @@
-export default (html, scope) => html`
-<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-
-<div id="editor">
-  <p>Hello World!</p>
-  <p>Some initial <strong>bold</strong> text</p>
-  <p><br></p>
-</div>
-`;
