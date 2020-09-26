8d2bc23ab1a8a790730573a5a2b7d9c09e37a2ed
sumbad <sumbad@ya.ru>
2020-09-26 15:52:05 +0800

    chore: create a draft for additional scripts

---
+++ scripts/h.sh
@@ -0,0 +1,12 @@
+# TODO: scripts for:
+
+# CLI
+    # "h:console": "hasura console --admin-secret myadminsecretkey",
+    # "h:migrate": "hasura migrate apply",
+    # "h:migrate-create": "hasura migrate create --from-server --admin-secret myadminsecretkey",
+    # "h:metadata": "hasura metadata export --admin-secret myadminsecretkey",
+    # "h:update-cli": "hasura update-cli"
+
+# Publish
+    # git remote -v
+    # git push heroku master
