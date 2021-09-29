946ce9a3d53256e692af018a06e1a7770b807b75
sumbad <sumbad@ya.ru>
2021-09-29 23:19:12 +0800

    [chore]: update webpack

--- src/hub/auth/auth.effect.ts
+++ src/hub/auth/auth.effect.ts
@@ -2,12 +2,11 @@ import { AUTH_ACTION, AuthAction } from 'hub/auth/auth.action';
 import hub from 'hub';
 import { Observable, of } from 'rxjs';
 import { filter, switchMap, map, catchError } from 'rxjs/operators';
-import { auth } from 'services/auth.service';
+import { auth } from 'services/Auth.service';
 
 export const auth$: Observable<AuthAction> = hub.$.pipe(
   filter((action) => AUTH_ACTION.AUTH === action.type),
   switchMap((d: AuthAction) => {
-    // debugger;
     return auth((d.payload as any).redirectUri).pipe(
       map(
         (payload): AuthAction => ({
