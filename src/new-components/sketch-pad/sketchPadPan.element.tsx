ac7d286fec3cd4bfca293203c91eedec5eee153e
sumbad <sumbad@ya.ru>
2022-08-26 16:53:07 +0800

    fix: SketchPadPan controls hide folding elements

--- src/new-components/sketch-pad/sketchPadPan.element.tsx
+++ src/new-components/sketch-pad/sketchPadPan.element.tsx
@@ -249,14 +249,12 @@ export const sketchPadPanElement = EG({
               position: sticky;
               top: 20px;
               width: 60px;
+              margin-top: 44px;
               margin-left: ${mode === 'expand' ? '-70px' : 0};
             `}
           >
             <div
               class="fab-container fab-container_main"
-              style={css`
-                margin-top: 1rem;
-              `}
             >
               <div class="fab-container fab-container_vertical">
                 <button class="fab-button fab-button_empty" onclick={updateCanvasContent}>
